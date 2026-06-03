d761dc422f91b329c5616c8df28c0cac1bc237e7
Nicolas Araya Ventura <150105875+nicolasarayaventura@users.noreply.github.com>
2026-06-03 12:27:58 -0400

    Update qc.R

--- results/task_1/seurat_scripts/qc.R
+++ results/task_1/seurat_scripts/qc.R
@@ -10,9 +10,11 @@ pbmc <- readRDS(file.path(data_dir, "pbmc_seurat_obj.rds"))
 # add mitochondrial % to metadata
 pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
 
-# save violin plot
-png(file.path(out_dir, "vlnplot_qc.png"), width = 1200, height = 400)
-VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+# save scatter plots
+plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+png(file.path(out_dir, "scatter_qc.png"), width = 1000, height = 500)
+print(plot1 + plot2)
 dev.off()
 
 # save scatter plots
@@ -27,7 +29,7 @@ dev.off()
 # less than 2500 probable doublets that have 2 cells in 1 droplet
 # less than 5% are for dying/ damaged cells
 pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
-pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000) #normalization with default paramaters for precaution
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
 
 # find top 2000 variable features
 pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
@@ -36,8 +38,8 @@ pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
 top10 <- head(VariableFeatures(pbmc), 10)
 plot1 <- VariableFeaturePlot(pbmc)
 plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
-png(file.path(out_dir, "variable_features.png"), width = 1200, height = 600)
-plot1 + plot2
+png(file.path(out_dir, "variable-features.png"), width = 1200, height = 600)
+print(plot1 + plot2)
 dev.off()
 
 # scale data across all genes
